125ce19bc928e6f209f89174a8a681094c2c375a
yangyucse <133809387+yangyucse@users.noreply.github.com>
2023-06-13 16:27:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 
 const configuration = new Configuration({
-    apiKey: process.env.VITE_OPENAI_API_KEY,
+    apiKey: proces.env.VITE_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
